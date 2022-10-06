0cf5123119cf90a9c386e452ea44973f2dc77a77
Aditya <aditya@passbird.co>
2022-10-06 16:44:16 +0530

    prettier-added

--- lambda/custom-nft/contracts/test/MetaKeepCertificate.js
+++ lambda/custom-nft/contracts/test/MetaKeepCertificate.js
@@ -1,8 +1,8 @@
-const {expect, assert} = require('chai');
-const {ethers} = require('hardhat');
-const {BigNumber} = require('ethers');
+const { expect, assert } = require("chai");
+const { ethers } = require("hardhat");
+const { BigNumber } = require("ethers");
 
-describe('Token', function () {
+describe("Token", function () {
   let MetaKeepCertificate;
   let certificate;
   let owner;
@@ -11,17 +11,19 @@ describe('Token', function () {
   let addrs;
 
   before(async function () {
-    MetaKeepCertificate = await ethers.getContractFactory('MetaKeepCertificates');
+    MetaKeepCertificate = await ethers.getContractFactory(
+      "MetaKeepCertificates"
+    );
     certificate = await MetaKeepCertificate.deploy("Test Token", "TT");
     [owner, addr1, addr2, ...addrs] = await ethers.getSigners();
   });
-  describe('Deployment', function () {
-    it('Should set the right owner', async function () {
+  describe("Deployment", function () {
+    it("Should set the right owner", async function () {
       expect(await certificate.owner()).to.equal(owner.address);
     });
   });
-  describe('Mint And Destroy Certificate', function () {
-    it('Should mint certificate', async function () {
+  describe("Mint And Destroy Certificate", function () {
+    it("Should mint certificate", async function () {
       await certificate.mint(addr1.address, 100, "bob", "bob@email.com");
       expect(await certificate.balanceOf(addr1.address)).to.equal(1);
     });
@@ -33,16 +35,20 @@ describe('Token', function () {
       expect(certificateData[2]).to.equal(100);
     });
 
-    it('Should not be able to transfer certificate as it is soulbound tokens', async function () {
-      await expect(certificate.connect(addr1).transferFrom(addr1.address,addr2.address, 100)).to.be.revertedWith('Cannot transfer');
+    it("Should not be able to transfer certificate as it is soulbound tokens", async function () {
+      await expect(
+        certificate
+          .connect(addr1)
+          .transferFrom(addr1.address, addr2.address, 100)
+      ).to.be.revertedWith("Cannot transfer");
     });
 
-    it('Should be able to destroy certificate', async function () {
+    it("Should be able to destroy certificate", async function () {
       await certificate.destroy(100);
       expect(await certificate.balanceOf(addr1.address)).to.equal(0);
     });
 
-    it('Should return null information if certificate is destroyed', async function () {
+    it("Should return null information if certificate is destroyed", async function () {
       const certificateData = await certificate.getCertificate(100);
       expect(certificateData[0]).to.equal("");
       expect(certificateData[1]).to.equal("");
