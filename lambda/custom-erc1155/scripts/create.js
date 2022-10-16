3aa5e4526583bce627c862bf4ba17da228ddbd2a
Aditya <aditya@passbird.co>
2022-10-16 19:19:20 +0530

    added-scripts-and-minor-changes

---
+++ lambda/custom-erc1155/scripts/create.js
@@ -0,0 +1,38 @@
+// Script to deploy Smart contract using Metakeep Lambda and REST API.
+import fs from "fs";
+import env from "dotenv";
+import { create } from "../../lambdaUtils.mjs";
+import getDeveloperWallet, {
+  checkAPIKey,
+  waitUntilTransactionMined,
+} from "../../../helpers/utils.mjs";
+
+async function main() {
+  env.config();
+
+  // Checks if the API_KEY is set in the .env file.
+  checkAPIKey();
+
+  const data = JSON.parse(
+    fs.readFileSync(
+      "../smart-contracts/artifacts/contracts/CustomERC1155.sol/CustomERC1155.json"
+    )
+  );
+  const developerAddress = await getDeveloperWallet();
+
+  const resultJson = await create(
+    ["Enter Your URI", developerAddress, "Custom ERC1155"],
+    data.abi,
+    data.bytecode
+  );
+
+  //Waits for the transaction to be mined.
+
+  await waitUntilTransactionMined(resultJson);
+
+  console.log(
+    "Lambda created successfully. Lambda address: " + resultJson.lambda
+  );
+}
+
+main();
