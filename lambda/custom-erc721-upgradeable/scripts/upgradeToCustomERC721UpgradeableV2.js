8192c043599b9ac79c5ccd9d975fae80133a4320
Aditya Dhir <102578238+aditya-passbird@users.noreply.github.com>
2023-05-22 13:31:59 +0530

    script-

---
+++ lambda/custom-erc721-upgradeable/scripts/upgradeToCustomERC721UpgradeableV2.js
@@ -0,0 +1,71 @@
+// Script to deploy Smart contract using MetaKeep Lambda REST API.
+import fs from "fs";
+import env from "dotenv";
+import { create, invoke } from "../../lambdaUtils.mjs";
+import getDeveloperWallet, {
+  checkAPIKey,
+  waitUntilTransactionMined,
+} from "../../../helpers/utils.mjs";
+
+async function deployCustomERC721UpgradeableV2() {
+  const data = JSON.parse(
+    fs.readFileSync(
+      "../smart-contracts/artifacts/contracts/CustomERC721UpgradeableV2.sol/CustomERC721UpgradeableV2.json"
+    )
+  );
+  const developerAddress = await getDeveloperWallet();
+
+  const resultJson = await create(
+    ["MetaKeepOriginalsV2", "MTKP", developerAddress],
+    data.abi,
+    data.bytecode
+  );
+
+  // Wait for the transaction to be mined.
+  await waitUntilTransactionMined(resultJson);
+
+  console.log(
+    "Upgradeable ERC721 Lambda created successfully. Lambda address: " +
+      resultJson.lambda
+  );
+
+  return resultJson.lambda;
+}
+
+async function upgradeToV2() {
+  env.config();
+
+  // Checks if the API_KEY is set in the .env file.
+  checkAPIKey();
+
+  // get the deployed lambda address
+  const lambdaAddress = await deployCustomERC721UpgradeableV2();
+
+  /* ******************** Upgrade To CustomERC721UpgradeableV2 ******************** */
+  console.log(
+    "******************** Upgrade To CustomERC721UpgradeableV2 ********************"
+  );
+  // Invokes the lambda function to upgrade the implementation contract.
+  console.log(
+    "Invoking lambda function to upgrade the implementation contract...\n"
+  );
+  const resultJson = await invoke(
+    "upgradeTo",
+    [lambdaAddress],
+    "Upgrade To CustomERC721UpgradeableV2",
+    process.env.CUSTOM_ERC721_PROXY_ADDRESS
+  );
+  console.log(
+    "Lambda invocation for upgrading the CustomERC721UpgradeableV2 is initiated: \n"
+  );
+
+  // Waits for the transaction to be mined.
+  await waitUntilTransactionMined(resultJson);
+  console.log(
+    "Lambda invocation for upgrading the CustomERC721Upgradeable is completed: " +
+      resultJson.transactionHash +
+      "\n"
+  );
+}
+
+upgradeToV2();
