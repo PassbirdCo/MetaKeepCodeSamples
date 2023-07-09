51b22288b1db819f35ffa9c91e380beaa67f2dd2
Aditya Dhir <102578238+aditya-passbird@users.noreply.github.com>
2023-07-09 15:32:27 +0530

    minor-fixes

--- lambda/custom-erc721/scripts/whitelistAndMint.js
+++ lambda/custom-erc721/scripts/whitelistAndMint.js
@@ -17,9 +17,31 @@ async function main() {
   // Gets the user Address to whitelist in NFT collection contract.
   const userAddress = await getUserWallet(process.env.USER_EMAIL);
 
-  /* ******************** Mint Token to User ******************** */
+  /* ******************** Whitelist User ******************** */
+  console.log("******************** Whitelist User ********************");
+  // Invokes the lambda function to whitelist the user.
+  console.log("Invoking lambda function to whitelist user...\n");
+  const resultJson = await invoke(
+    "addToWhitelist",
+    [userAddress],
+    "Whitelist User"
+  );
+  console.log("Lambda invocation for whitelisting user is initiated: \n");
+
+  // Waits for the transaction to be mined.
+  await waitUntilTransactionMined(resultJson);
   console.log(
-    "******************** Mint Token to User ********************"
+    "Lambda invocation for whitelisting user is completed: " +
+      resultJson.transactionHash +
+      "\n"
+  );
+
+  // Waits for 5 seconds.
+  sleep(5000);
+
+  /* ******************** Mint Token to Whitelisted User ******************** */
+  console.log(
+    "******************** Mint Token to Whitelisted User ********************"
   );
 
   // Invokes the lambda function to mint token for the user.
