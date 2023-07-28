b071703068aa0dc76a8c24674dc40f14415e9503
Aditya Dhir <102578238+aditya-passbird@users.noreply.github.com>
2023-07-28 16:19:27 +0530

    fixes

--- lambda/aa-wallet/backend/constants.js
+++
@@ -1,8 +0,0 @@
-// The Registration Smart Contract Method in the ../smart-contracts/contracts/Voting.sol
-export const REGISTRATION_FUNCTION_NAME = "registerCandidate";
-
-// The Voting Smart Contract Method in the ../smart-contracts/contracts/Voting.sol
-export const VOTING_FUNCTION_NAME = "voteForCandidate";
-
-// The Get Candidate Details Smart Contract Method in ../smart-contracts/contracts/Voting.sol
-export const GET_CANDIDATE_FUNCTION_NAME = "getCandidate";
