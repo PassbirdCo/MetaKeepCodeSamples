8f1e380ac48531bec503035c4ceae210fb91a4f9
Rohan Sharma <rohan@passbird.co>
2023-11-23 18:34:32 -0800

    Library Init

---
+++ other-platforms/react-native/MetaKeepReactNativeSDK/android/src/main/java/com/metakeepreactnativesdk/MetaKeepReactNativeSDKPackage.kt
@@ -0,0 +1,17 @@
+package com.metakeepreactnativesdk
+
+import com.facebook.react.ReactPackage
+import com.facebook.react.bridge.NativeModule
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.uimanager.ViewManager
+
+
+class MetaKeepReactNativeSDKPackage : ReactPackage {
+  override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
+    return listOf(MetaKeepReactNativeSDKModule(reactContext))
+  }
+
+  override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<*, *>> {
+    return emptyList()
+  }
+}
