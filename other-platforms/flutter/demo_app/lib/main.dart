2e00a614eaeaa66eb34aa7b9b6ccd7c0ca0bf0a8
Rohan Sharma <rohan@passbird.co>
2024-01-23 15:58:20 -0800

    Fixes

--- other-platforms/flutter/demo_app/lib/main.dart
+++ other-platforms/flutter/demo_app/lib/main.dart
@@ -26,10 +26,6 @@ class MyApp extends StatelessWidget {
         // reload" button in a Flutter-supported IDE, or press "r" if you used
         // the command line to start the app).
         //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
@@ -59,8 +55,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  // int _counter = 0;
-
   // Store the MetaKeep SDK app ID.
   String? _appId;
 
