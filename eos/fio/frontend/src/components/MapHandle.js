2875b369a466eca87152c3bac26f94b356c74c7a
Mohit Harshan <mohit@metakeep.xyz>
2024-03-13 16:37:45 +0530

    added test code

---
+++ eos/fio/frontend/src/components/MapHandle.js
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import "./common.css";
+import { message } from "antd";
+const { mapHandle } = require("metakeep-fio");
+
+const MapHandle = () => {
+  const [email, setEmail] = useState("");
+  const [publicAddress, setPublicAddress] = useState("");
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handleMapHandle = async () => {
+    if (!email) {
+      message.error("Please enter your email!");
+      return;
+    }
+
+    if (!publicAddress) {
+      message.error("Please enter your public address!");
+      return;
+    }
+
+    mapHandle({
+      appId: process.env.REACT_APP_APP_ID,
+      publicAddress: publicAddress,
+      email,
+      chainCode: "ETH",
+      tokenCode: "ETH",
+    });
+  };
+
+  return (
+    <div className="form-container">
+      <h2>Map handle</h2>
+      <input
+        type="email"
+        value={email}
+        onChange={handleEmailChange}
+        placeholder="Enter your email"
+        className="email-input"
+      />
+      <input
+        value={publicAddress}
+        onChange={(e) => setPublicAddress(e.target.value)}
+        placeholder="Enter your public address"
+        className="email-input"
+      />
+
+      <button onClick={handleMapHandle} className="register-button">
+        {"Register"}
+      </button>
+    </div>
+  );
+};
+
+export default MapHandle;
