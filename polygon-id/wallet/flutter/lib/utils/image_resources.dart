ddda0bd02956868edaae82a7c7bae9507ddb0f06
Aditya Dhir <102578238+aditya-passbird@users.noreply.github.com>
2024-07-15 16:49:24 +0530

    Update image_resources.dart

--- polygon-id/wallet/flutter/lib/utils/image_resources.dart
+++ polygon-id/wallet/flutter/lib/utils/image_resources.dart
@@ -2,7 +2,7 @@ class ImageResources {
   ImageResources._();
 
   static const String logo = "assets/images/polygon_id_logo.svg";
-    static const String metakeepCryptographyLogo =
+  static const String metakeepCryptographyLogo =
       "assets/images/metakeep-cryptography-light.png";
   static const String claimBackground = "assets/images/claim_bg.svg";
 }
