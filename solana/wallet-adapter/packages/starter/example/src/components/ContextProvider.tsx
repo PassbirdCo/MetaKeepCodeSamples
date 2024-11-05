1cf5738583edcda1e5959b2baaa63fb2064dd4e0
aditya-passbird <aditya@passbird.co>
2024-11-05 14:16:20 +0530

    fix

--- solana/wallet-adapter/packages/starter/example/src/components/ContextProvider.tsx
+++ solana/wallet-adapter/packages/starter/example/src/components/ContextProvider.tsx
@@ -6,7 +6,7 @@ import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
 import { WalletDialogProvider as MaterialUIWalletDialogProvider } from '@solana/wallet-adapter-material-ui';
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
 import { WalletModalProvider as ReactUIWalletModalProvider } from '@solana/wallet-adapter-react-ui';
-import { UnsafeBurnerWalletAdapter, MetaKeepWalletAdapter, MathWalletAdapterConfig } from '@solana/wallet-adapter-wallets';
+import { MetaKeepWalletAdapter } from '@solana/wallet-adapter-wallets';
 import { type SolanaSignInInput } from '@solana/wallet-standard-features';
 import { verifySignIn } from '@solana/wallet-standard-util';
 import { clusterApiUrl } from '@solana/web3.js';
@@ -61,7 +61,6 @@ const WalletContextProvider: FC<{ children: ReactNode }> = ({ children }) => {
 
     const wallets = useMemo(
         () => [
-
             new MetaKeepWalletAdapter({
                 appId: 'a12ab037-9936-4f83-ac27-8be854f25abc',
                 network: 'devnet',
@@ -79,7 +78,6 @@ const WalletContextProvider: FC<{ children: ReactNode }> = ({ children }) => {
              * instantiate its legacy wallet adapter here. Common legacy adapters can be found
              * in the npm package `@solana/wallet-adapter-wallets`.
              */
-            new UnsafeBurnerWalletAdapter(),
         ],
         // eslint-disable-next-line react-hooks/exhaustive-deps
         [network]
